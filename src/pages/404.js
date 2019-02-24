3939763f0d3851d0513784bc01b9e3eac66dec54
vissejul <vissejul@students.zhaw.ch>
2019-02-24 13:45:57 +0100

    added header to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Layout from '../layouts/layout/layout';
+import BlogHeader from '../components/BlogHeader/BlogHeader';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
+    <BlogHeader />
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>

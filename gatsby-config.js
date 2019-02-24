1bbf60cc22096f7e0b75bc0f3f89b945211a75eb
vissejul <vissejul@students.zhaw.ch>
2019-02-24 13:45:05 +0100

    fixed using images in markdown.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,19 @@ module.exports = {
         path: `${__dirname}/content/blog`
       }
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          }
+        ]
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {

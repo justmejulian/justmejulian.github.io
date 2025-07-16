095c56425bd2975b7eaecf1817925de3d0f85fe8
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-16 15:56:43 +0200

    set global styles in layout

--- src/components/preact/Button.tsx
+++ src/components/preact/Button.tsx
@@ -3,14 +3,7 @@ interface ButtonProps {
 }
 
 function Button({ title }: ButtonProps) {
-  return (
-    <button
-      className="inline-block rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:ring-3 focus:outline-hidden"
-      onClick={() => console.log('Button clicked')}
-    >
-      {title}
-    </button>
-  );
+  return <button onClick={() => console.log('Button clicked')}>{title}</button>;
 }
 
 export default Button;
